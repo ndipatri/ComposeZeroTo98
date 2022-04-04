6c6e280a3962ff6433c4b3cbb33655a3691f86ae
ndipatri <ndipatri@gmail.com>
2022-04-04 06:04:10 -0400

    no message

--- app/src/androidTest/java/com/ndipatri/iot/zeroto98/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/ndipatri/iot/zeroto98/ExampleInstrumentedTest.kt
@@ -1,12 +1,12 @@
 package com.ndipatri.iot.zeroto98
 
-import androidx.test.platform.app.InstrumentationRegistry
+import android.content.Intent
+import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +15,28 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    val rule = createComposeRule()
+
+    @get:Rule
+    val activityTestRule = ActivityTestRule(
+        MainActivity::class.java,
+        false,
+        false
+    )
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.ndipatri.iot.zeroto98", appContext.packageName)
+    fun showCurrentRedSirenState_off() {
+
+        activityTestRule.launchActivity(Intent())
+
+        // We need to add sleep because we are making background calls in the app
+        // to communicate with our real-world siren.  This takes time.  We don't know the
+        // app well enough to be able to create the necessary 'Espresso Idling Resource' hooks
+        Thread.sleep(5000)
+
+        // This only works if the real system is in the 'off' state.
+        onView(withId(R.id.textview_first)).check(matches(withText("RedSiren is off!")))
     }
 }
